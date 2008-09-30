84113e9187f0eb23568925975551ebe2ec0d3274
Marcus Povey <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-09-30 18:53:04 +0000

    Added first draft COPPA plugin

---
+++ languages/en.php
@@ -0,0 +1,18 @@
+<?php
+	/**
+	 * Elgg coppa language pack
+	 * 
+	 * @package ElggCOPPA
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 */
+
+	$english = array(
+		'coppa:text' => 'I am over 13 years of age',
+		'coppa:fail' => 'You must be over 13 years of age to register for this website',
+	);
+					
+	add_translation("en",$english);
+?>
---
+++ start.php
@@ -0,0 +1,28 @@
+<?php
+	/**
+	 * Elgg COPPA plugin
+	 * 
+	 * @package ElggCOPPA
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 */
+
+	function coppa_init()
+	{
+		register_plugin_hook("action", "register", "coppa_register");
+	}
+	
+	function coppa_register($hook, $entity_type, $returnvalue, $params)
+	{
+		$coppa = get_input('coppa');
+		if ($coppa)
+			return true;
+			
+		register_error('coppa:fail');
+		return false;
+	}
+	
+	register_elgg_event_handler('init','system','coppa_init');
+?>
